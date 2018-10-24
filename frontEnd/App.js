cf5e381808e779c5fc50c3a89b63a5b1f5055790
JimTheBush <zygimantasaugunas@gmail.com>
2018-10-24 22:34:51 +0300

    Played around a little bit

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -9,6 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -21,7 +22,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native yeee!</Text>
+        <Text style={styles.welcome}>Get ready!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
