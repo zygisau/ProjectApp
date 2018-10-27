18833fa9ba13f599d0a5efef2633a2c8faf4e5a5
JimTheBush <zygimantasaugunas@gmail.com>
2018-10-27 15:29:41 +0300

    Hello

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ff2523',
+    backgroundColor: '#ff00bf',
   },
   welcome: {
     fontSize: 20,
