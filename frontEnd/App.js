e484ee7377cedab902de9c2c1194d769711ecf9b
JimTheBush <zygimantasaugunas@gmail.com>
2018-10-31 11:41:48 +0200

    Login update

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -11,6 +11,10 @@ export default class App extends Component<Props> {
   render() {
     return (
           <ImageBackground style={styles.container} source={require('./assets/images/bg1.jpg')}>
+              <View style={styles.top}>
+                  <Image style={styles.logo} source={require('./assets/logos/logo.svg')} />
+              </View>
+              <View style={styles.middle}>
               <Button
                   buttonStyle={styles.buttonFacebook}
                   title='CONTINUE WITH FACEBOOK'
@@ -19,11 +23,18 @@ export default class App extends Component<Props> {
                   buttonStyle={styles.buttonGoogle}
                   title='CONTINUE WITH GOOGLE'
                   textStyle={styles.btText}  />
-              <Text style={styles.already}>Already a user?</Text>
               <Button
-                  buttonStyle={styles.buttonLog}
-                  title='LOG IN'
+                  buttonStyle={styles.buttonSign}
+                  title='SIGN UP'
                   textStyle={styles.btText}  />
+              </View>
+              <View style={styles.bottom}>
+                  <Text style={styles.already}>Already a user?</Text>
+                  <Button
+                      buttonStyle={styles.buttonLog}
+                      title='LOG IN'
+                      textStyle={styles.btText}  />
+              </View>
           </ImageBackground>
     );
   }
@@ -32,57 +43,94 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    //flexDirection: 'column',
+    //justifyContent: 'space-around',
+    //alignItems: 'stretch',
+    backgroundColor: '#f5fcff',
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
   },
-  already: {
-      position: 'absolute',
+  top: {
+      backgroundColor: '#f5fcff',
+      flex: 0.4,
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      //top: '10%',
+      //resizeMode: 'cover',
+  },
+  logo: {
+      width: 210,
+      height: 210,
+      flexDirection: 'row',
+      alignItems: 'center',
+  },
+  middle: {
+      flex: 0.3,
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'stretch',
+      //top: '25%',
       width: '100%',
       height: '100%',
-      top: '73%',
+      resizeMode: 'cover',
+  },
+  already: {
+      //position: 'absolute',
+      //width: '100%',
+      //height: '100%',
+      //top: '73%',
       textAlign: 'center',
       color: '#f5fcff',
       padding: 0,
       margin: 0,
-      fontWeight: 'bold',
+      //fontWeight: 'bold',
       fontSize: 20,
       fontFamily: Fonts.FranklinGothic,
   },
   btText: {
-      position: 'absolute',
-      justifyContent: 'center',
-      alignItems: 'center',
-      letterSpacing: 10,
-      fontWeight: 'bold',
-      fontSize: 25,
+      letterSpacing: 5,
+      textAlign: 'center',
+      fontSize: 19,
       fontFamily: Fonts.FranklinGothic,
   },
   buttonFacebook: {
-      position: 'relative',
-      width: '140%',
-      height: '25%',
-      backgroundColor: '#2e2f2e',
-      borderRadius:100,
-      top: '10%',
+      height: 55,
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#3b5998',
+      borderRadius: 100,
   },
   buttonGoogle: {
-      position: 'relative',
-      width: '140%',
-      height: '25%',
-      backgroundColor: '#2e2f2e',
+      height: 55,
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#db4437',
       borderRadius: 100,
-      top: '10%',
   },
+  buttonSign: {
+      height: 55,
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#6a9500',
+      borderRadius: 100,
+  },
+  bottom: {
+      flex: 0.3,
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      alignItems: 'stretch',
+      //top: '30%',
+      width: '100%',
+      height: '100%',
+      resizeMode: 'cover',
+   },
   buttonLog: {
-      position: 'relative',
-      width: '140%',
-      height: '25%',
+      height: 55,
       backgroundColor: '#2e2f2e',
       borderRadius:100,
-      top: '30%',
+      //top: '30%',
   }
+
 });
