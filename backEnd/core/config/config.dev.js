ff80e9df127daaa0080c7f19aa80f365a1f7a38f
Ignas Dailydė <ignas.dailyde@gmail.com>
2018-11-06 09:21:57 +0200

    3000 port

--- backEnd/core/config/config.dev.js
+++ backEnd/core/config/config.dev.js
@@ -7,6 +7,6 @@ config.logFileName = 'app.log';
 config.dbHost = process.env.dbHost || 'localhost';
 config.dbPort = process.env.dbPort || '27017';
 config.dbName = process.env.dbName || 'db';
-config.serverPort = process.env.serverPort || 3002;
+config.serverPort = process.env.serverPort || 3000;
 
 export default config;
