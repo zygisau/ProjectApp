1d689defd2c50451f40cfe33b049224c37a6e179
JimTheBush <zygimantasaugunas@gmail.com>
2018-11-12 10:46:37 +0200

    Last update before presentation

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -1,159 +1,40 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
+import {Alert, StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
 import { Button } from 'react-native-elements';
 import { Fonts } from "./utils/fonts";
-//import Window from './components/compLogin';
-
-//const bg = '.assets/bg.jpg';
+import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+import compLoginScreen from './components/screens/compLogin';
+import compSignUpScreen from  "./components/screens/compSignUp";
+import compLoginHomeScreen from "./components/screens/compLoginHome";
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-          <ImageBackground style={styles.container} source={require('./assets/images/bg3.jpg')}>
-              <View style={styles.top}>
-                  <Image style={styles.logo} source={require('./assets/logos/logo.png')} />
-              </View>
-              <View style={styles.middle}>
-              <Button
-                  buttonStyle={styles.buttonFacebook}
-                  title='CONTINUE WITH FACEBOOK'
-                  textStyle={styles.btText}  />
-              <Button
-                  buttonStyle={styles.buttonGoogle}
-                  title='CONTINUE WITH GOOGLE'
-                  textStyle={styles.btText}  />
-              <Button
-                  buttonStyle={styles.buttonSign}
-                  title='SIGN UP'
-                  textStyle={styles.btText}  />
-              </View>
-              <View style={styles.bottom}>
-                  <Text style={styles.already}>Already a user?</Text>
-                  <Button
-                      buttonStyle={styles.buttonLog}
-                      title='LOG IN'
-                      textStyle={styles.btText}  />
-              </View>
-              <View style={styles.language}>
-                  <Button
-                      buttonStyle={styles.buttonLanguage}
-                      title='CHANGE LANGUAGE'
-                      //clear='0'
-                      textStyle={styles.btText}  />
-              </View>
-          </ImageBackground>
-    );
-  }
+//export default class App extends React.Component {
+    render() {
+        return <AppStackNavigator />
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //flexDirection: 'column',
-    //justifyContent: 'space-around',
-    //alignItems: 'stretch',
-    backgroundColor: '#f5fcff',
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-  },
-  top: {
-      //backgroundColor: '#f5fcff',
-      flex: 0.4,
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      //top: '10%',
-      //resizeMode: 'cover',
-  },
-  logo: {
-      width: 230,
-      height: 214,
-      flexDirection: 'row',
-      alignItems: 'center',
-  },
-  middle: {
-      flex: 0.3,
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      alignItems: 'stretch',
-      //top: '25%',
-      width: '100%',
-      height: '100%',
-      resizeMode: 'cover',
-  },
-  already: {
-      //position: 'absolute',
-      //width: '100%',
-      height: 40,
-      //top: '73%',
-      textAlign: 'center',
-      color: '#000000',
-      padding: 0,
-      margin: 0,
-      //fontWeight: 'bold',
-      fontSize: 20,
-      fontFamily: Fonts.FranklinGothic,
-  },
-  btText: {
-      letterSpacing: 5,
-      textAlign: 'center',
-      fontSize: 19,
-      fontFamily: Fonts.FranklinGothic,
-  },
-  buttonFacebook: {
-      height: 55,
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: '#3b5998',
-      borderRadius: 100,
-  },
-  buttonGoogle: {
-      height: 55,
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: '#db4437',
-      borderRadius: 100,
-  },
-  buttonSign: {
-      height: 55,
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: '#6a9500',
-      borderRadius: 100,
-  },
-  bottom: {
-      flex: 0.25,
-      flexDirection: 'column',
-      //justifyContent: 'space-around',
-      alignItems: 'stretch',
-      top: '5%',
-      width: '100%',
-      height: '100%',
-      resizeMode: 'cover',
-   },
-  buttonLog: {
-      height: 55,
-      backgroundColor: '#2e2f2e',
-      borderRadius:100,
-      //top: '30%',
-  },
-  language: {
-      flex: 0.15,
-      flexDirection: 'column',
-      //justifyContent: 'space-around',
-      alignItems: 'flex-end',
-      top: '5%',
-      right: 0,
-      //width: '80%',
-      //height: '100%',
-      resizeMode: 'cover',
-  },
-  buttonLanguage: {
-      height: 55,
-      backgroundColor: '#2e2f2e',
-      borderRadius:100,
-  }
+const AppStackNavigator = createStackNavigator(
+    {
+        //LoginHome: compLoginHomeScreen,
+        Login: compLoginHomeScreen
+    },
+    {
+        initialRouteName: 'Login',
+    }
+);
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //flexDirection: 'column',
+        //justifyContent: 'space-around',
+        //alignItems: 'stretch',
+        backgroundColor: '#f5fcff',
+        color: '#000000',
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    }
 });
---
+++ frontEnd/components/screens/compLogin.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+import {Alert, StyleSheet, Text, View, Image, ImageBackground, TextInput, ScrollView} from 'react-native';
+import {FormLabel, FormInput, FormValidationMessage, Button} from 'react-native-elements';
+import { Fonts } from "../../utils/fonts";
+import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+
+export default class compLoginScreen extends Component {
+
+    static navigationOptions = {
+        header: null
+    }
+    constructor(props) {
+        super(props)
+        this.function = this.function.bind(this);
+    }
+    function() {
+
+    };
+    render() {
+        return (
+            <ImageBackground style={styles.container} source={require('../../assets/images/bg3.jpg')}>
+                <View style={styles.logIn}>
+                    <Text style={styles.logInText}>SIGN UP</Text>
+                </View>
+                <View style={styles.logInputs}>
+                    <FormLabel>NAME</FormLabel>
+                    <FormInput onChangeText={this.function()}/>
+                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+
+                    <FormLabel>PASSWORD</FormLabel>
+                    <FormInput onChangeText={this.function()}/>
+                    <FormValidationMessage>{'This field is required'}</FormValidationMessage>
+                </View>
+                <View style={styles.bottom}>
+                    <Button
+                        buttonStyle={styles.buttonLog}
+                        title='LOG IN'
+                        textStyle={styles.btText}
+                    />
+                </View>
+            </ImageBackground>
+        )
+    }
+}
+/*
+export default createStackNavigator({
+    Login: compLoginScreen
+});
+*/
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        //justifyContent: 'space-around',
+        //alignItems: 'stretch',
+        backgroundColor: '#f5fcff',
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    //
+    logInputs: {
+        justifyContent: 'flex-end',
+        flexDirection: 'column',
+        flex: 0.35,
+        textAlign: 'left',
+        //left: 30,
+    },
+    logIn: {
+        justifyContent: 'flex-end',
+        //flexDirection: 'column',
+        flex: 0.3,
+        textAlign: 'left',
+        left: 30,
+    },
+    logInText: {
+        fontSize: 50,
+        color: '#000000',
+        fontWeight: 'bold',
+        fontFamily: Fonts.FranklinGothic,
+    },
+    //
+    bottom: {
+        flex: 0.25,
+        flexDirection: 'column',
+        //justifyContent: 'space-around',
+        alignItems: 'stretch',
+        top: '5%',
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    buttonLog: {
+        height: 55,
+        backgroundColor: '#2e2f2e',
+        borderRadius:100,
+        //top: '30%',
+    },
+    btText: {
+        letterSpacing: 5,
+        textAlign: 'center',
+        fontSize: 19,
+        fontFamily: Fonts.FranklinGothic,
+    },
+});
