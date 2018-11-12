fbd46d5997c24a63950b9a867167dfe5fcd41d22
ToNyHasK <43828825+ToNyHasK@users.noreply.github.com>
2018-11-12 21:55:54 +0200

    Padaryta keli mygtukai, also, perejimas i kita puslapi.

---
+++ frontEnd/components/DetailsScreen.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+class DetailsScreen extends Component {
+    static navigationOptions = {
+        title: 'Details',
+    };
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Details Screen</Text>
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details')}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
+            </View>
+        );
+    }
+}
+
+export default DetailsScreen;
