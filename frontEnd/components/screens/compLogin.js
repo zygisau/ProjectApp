85701335fea2ed4b4c430f767087260bb1c85f6f
JimTheBush <zygimantasaugunas@gmail.com>
2018-12-27 22:02:45 +0200

    Yes

--- frontEnd/components/screens/compLogin.js
+++ frontEnd/components/screens/compLogin.js
@@ -51,8 +51,8 @@ class CompLoginScreen extends PureComponent {
             password: this.state.password,
         };
         console.log({params});
-        fetch("http://192.168.0.101:3000/api/v1/authenticate", {
-        //fetch("http://206.189.4.112:3000/api/v1/authenticate", {
+        //fetch("http://192.168.0.101:3000/api/v1/authenticate", {
+        fetch("http://206.189.4.112:3000/api/v1/authenticate", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=UTF-8'
